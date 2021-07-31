6c1a621d5a2b2ae3f116ed5f124d015badef51de
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-07-31 12:03:45 +0800

    Rewite with DI.

--- EpicBundle-FreeGames-dotnet/Program.cs
+++ EpicBundle-FreeGames-dotnet/Program.cs
@@ -1,203 +1,71 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using HtmlAgilityPack;
-using ScrapySharp.Extensions;
+using Microsoft.Extensions.Configuration;
+using NLog;
+using NLog.Extensions.Logging;
 
 namespace EpicBundle_FreeGames_dotnet {
 	class Program {
-		#region DI varialbles
-		private readonly ILogger _logger;
-		#endregion
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+		private static readonly IConfigurationRoot config = new ConfigurationBuilder()
+				   .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+				   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				   .Build();
+		private static readonly string URL = "https://www.epicbundle.com/category/article/for-free/";
 
-		#region static varialbles
-		private readonly string URL = "https://www.epicbundle.com/category/article/for-free/";
-		private readonly string configPath = "config.json";
-		private readonly string recordPath = "record.json";
-		private readonly List<string> wordList = new List<string> { 
-			"humble choice bundle",
-			"follow us on facebook", 
-			"more games for free here", 
-			"\"limitless $10 epic coupons\"", 
-			"add your comment." 
-		};
-		#endregion
-
-		public Program(ILogger<Program> logger) {
-			_logger = logger;
-		}
-
-		private static void ConfigureServices(ServiceCollection service) {
-			service.AddLogging(logging => {
-				logging.AddFilter("System", LogLevel.Warning);
-				logging.AddFilter("Microsoft", LogLevel.Warning);
-				logging.AddConsole();
-			}).AddTransient<Program>();
-		}
-
-		internal async Task SendNotification(string id, string token, List<string> msgs) {
-			if (msgs.Count == 0) {
-				_logger.LogDebug("No new notifications !");
-				return;
-			}
-
-			using var myBot = new TgBot(token);
-			int count = 1;
-			foreach (var msg in msgs) {
-				_logger.LogDebug("Sending Message {0}", count++);
-				await myBot.SendMessage(
-					chatId: id,
-					msg: msg,
-					htmlMode: true
-				);
-			}
-		}
-
-		internal List<string> TryGetLinks(string url) {
-			var results = new List<string>();
-
-			#region getting all possible links
-			_logger.LogDebug("Getting article page source");
-			var webGet = new HtmlWeb();
-			var htmlDoc = webGet.Load(url);
-			_logger.LogDebug("Debug");
-
-			var links = htmlDoc.DocumentNode.CssSelect("div.entry-content a");
-			#endregion
-
-			#region add links to list
-			foreach (var each in links) {
-				if (!wordList.Exists(x => x == each.InnerText.ToString().ToLower())) {
-					string link = each.Attributes["href"].Value.ToString().Split('?')[0];
-
-					if (link.Contains("#disqus_thread"))
-						continue;
-
-					_logger.LogInformation("Get possible link: {0}", link);
-					results.Add(link);
-				}
-			}
-			#endregion
-
-			return results;
-		} 
-
-		internal async Task ProcessingData(HtmlDocument htmlDoc, List<Dictionary<string, string>> records, string chatId, string token) {
-			var pushList = new List<string>();
-			var recordList = new List<Dictionary<string, string>>();
-
-			var articles = htmlDoc.DocumentNode.CssSelect("div.post-column article header h2 a");
-
-			foreach (var each in articles) {
-				#region get article titles and links
-				var title = each.InnerText.ToString();
-				var link = each.Attributes["href"].Value.ToString();
-
-				_logger.LogInformation("Found new info: {0}", title);
-				#endregion
-
-				#region save titles and links to List
-				var tmp = new Dictionary<string, string> {
-					{ "title", title },
-					{ "url", link}
-				};
-				recordList.Add(tmp);
-				#endregion
-
-				#region decide if push or not
-				bool isPush = true;
-				foreach (var i in records)
-					if (i["title"] == title && i["url"] == link)
-						isPush = false;
-				#endregion
-
-				#region push code
-				if (isPush) {
-					_logger.LogInformation("Add {0} to push list", title);
-
-					//try to get links
-					var possibleLinks = TryGetLinks(url: link);
-
-					var tmpstr = "<b>EpicBundle 信息</b>\n\n";
-					tmpstr += "<i>" + title + "</i>\n";
-					tmpstr += "文章链接: " + link + "\n";
-					tmpstr += "可能的领取链接:\n";
-
-					foreach (var i in possibleLinks)
-						tmpstr += i + "\n";
-
-					pushList.Add(tmpstr);
-				}
-				#endregion
-			}
-
-			#region send notifications
-			_logger.LogInformation("Sending notifications");
-			await SendNotification(id: chatId, token: token, msgs: pushList);
-			_logger.LogInformation("Done");
-			#endregion
-
-			#region writing records
-			_logger.LogInformation("Writing records");
-			using var jsonop = new JsonOP();
-			try {
-				jsonop.WriteData(data: recordList, path: recordPath);
-			} catch (Exception ex) {
-				_logger.LogError("Writing records failed!");
-				_logger.LogError("Error message: {0}", ex.Message);
-			}
-			#endregion
-
-		}
-
-		internal async Task Run() {
-			_logger.LogInformation(DateTime.Now.ToString() + " - Start Job - ");
-
-			var records = new List<Dictionary<string, string>>();
-			var config = new Dictionary<string, string>();
-
-			#region load data
-			using (var jsonop = new JsonOP()) {
-				_logger.LogInformation("Loading previous records");
-				try {
-					records = jsonop.LoadData(recordPath);
-				} catch (Exception ex) {
-					_logger.LogError("Error loading previous records!");
-					_logger.LogError("Error message: {0}", ex.Message);
-				}
-				_logger.LogInformation("Done");
-
-				_logger.LogInformation("Loading configs");
-				try {
-					config = jsonop.LoadConfig(configPath);
-				} catch (Exception ex) {
-					_logger.LogError("Error loading configs!");
-					_logger.LogError("Error message: {0}", ex.Message);
-				}
-				_logger.LogInformation("Done");
-			}
-			#endregion
-
-			_logger.LogInformation("Getting page source");
-			var webGet = new HtmlWeb();
-			var htmlDoc = webGet.Load(URL);
-			_logger.LogInformation("Done");
-
-			_logger.LogInformation("Start processing data");
-			await ProcessingData(htmlDoc: htmlDoc, records: records, chatId: config["CHAT_ID"], token: config["TOKEN"]);
-			_logger.LogInformation("Done");
-
-			_logger.LogInformation(DateTime.Now.ToString() + " - End Job - ");
+		public static IServiceProvider BuildDi() {
+			return new ServiceCollection()
+				.AddTransient<Scraper>()
+				.AddTransient<Parser>()
+				.AddTransient<TgBot>()
+				.AddTransient<JsonOP>()
+				.AddLogging(loggingBuilder => {
+					// configure Logging with NLog
+					loggingBuilder.ClearProviders();
+					loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+					loggingBuilder.AddNLog(config);
+				})
+			   .BuildServiceProvider();
 		}
 
 		static async Task Main() {
-			var services = new ServiceCollection();
-			ConfigureServices(services);
-			using ServiceProvider serviceProvider = services.BuildServiceProvider();
-			Program app = serviceProvider.GetService<Program>();
-			await app.Run();
+			try {
+				var servicesProvider = BuildDi();
+
+				logger.Info(" - Start Job -");
+
+				using (servicesProvider as IDisposable) {
+					// Get telegram bot token, chatID and previous records
+					var jsonOp = servicesProvider.GetRequiredService<JsonOP>();
+					var tgConfig = jsonOp.LoadConfig(); // token, chatID
+					var oldRecords = jsonOp.LoadData(); // old records
+
+					// Get page source
+					var scraper = servicesProvider.GetRequiredService<Scraper>();
+					var source = scraper.GetHtmlSource(URL);
+
+					// Parse page source
+					var parser = servicesProvider.GetRequiredService<Parser>();
+					var parseResult = parser.Parse(source, oldRecords);
+					var pushList = parseResult.Item1; // notification list
+					var recordList = parseResult.Item2; // new records list
+
+					// Write new records
+					jsonOp.WriteData(recordList);
+
+					//Send notifications
+					var tgBot = servicesProvider.GetRequiredService<TgBot>();
+					await tgBot.SendMessage(token: tgConfig["TOKEN"], chatID: tgConfig["CHAT_ID"], pushList, htmlMode: true);
+				}
+
+				logger.Info(" - Job End -\n\n");
+			} catch (Exception ex) {
+				logger.Error("{0}\n\n", ex.Message);
+			} finally {
+				LogManager.Shutdown();
+			}
 		}
 	}
 }
---
+++ EpicBundle-FreeGames-dotnet/Scraper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
+
+namespace EpicBundle_FreeGames_dotnet {
+	class Scraper: IDisposable {
+		private readonly ILogger<Scraper> _logger;
+
+		public Scraper(ILogger<Scraper> logger) {
+			_logger = logger;
+		}
+
+		public HtmlDocument GetHtmlSource(string url) {
+			try {
+				_logger.LogDebug("Getting page source");
+				var webGet = new HtmlWeb();
+				var htmlDoc = webGet.Load(url);
+				_logger.LogDebug("Done");
+				return htmlDoc;
+			} catch (Exception) {
+				_logger.LogError("Scraping Error");
+				throw;
+			} finally {
+				Dispose();
+			}
+		}
+
+		public void Dispose() {
+			GC.SuppressFinalize(this);
+		}
+	}
+}
